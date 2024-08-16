f2820f5a158167014114a1291f71f80f97884094
TBS96 <9tbs6@proton.me>
2024-08-16 20:52:17 +0530

    import { addTrack, setupPlayer } from musicPlayerServices.js ; useState(), useEffect() ; basic rendering parts done
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { addTrack, setupPlayer } from '../musicPlayerServices';
+
 
 const App = () => {
+
+  const [isPlayerReady, setIsPLayerReady] = useState(false);
+
+  const setup = async() => {
+    let isSetUp = await setupPlayer();
+    if(isSetUp)
+    {
+      await addTrack();
+    }
+    setIsPLayerReady(isSetUp);
+  };
+
+  useEffect(() => {
+    setup();
+  }, [])
+
+  if(!isPlayerReady)
+  {
+    return(
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
+      <StatusBar />
       <Text>App</Text>
     </View>
   )
 }
 
-export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+export default App
